3dd4606987f29f7185a04e2fea3d071ce96eff6b
shaikhaleema200802-lab <shaikhaleema200802@gmail.com>
2026-07-12 09:56:14 +0530

    Create family.java

---
+++ family.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+class Family
+{
+ public static void main(String[] args)
+{
+ System.out.println("Father: ms Rahamathulla");
+ System.out.println("mother:ms hajeera");
+System.out.println("sister:shajarin");
+System.out.println("brother:sadiq");
+}
